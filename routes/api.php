6c00d4c13e31a24ad463571b2d3791e765c4863e
MahmoudDabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2023-07-30 13:40:14 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,9 @@ use App\Http\Controllers\Api\TagController;
 
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
-Route::get('/verify', [AuthController::class, 'verify']);
 Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::get('/verify', [AuthController::class, 'verify']);
     Route::apiResource('/tags', TagController::class);
 
     Route::get('/posts/trash', [PostController::class, 'trash']);
